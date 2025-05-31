fdfe39fc7cca56198253510f0a6a799fd2279940
341_from_mac <838839085@qq.com>
2025-05-31 18:06:35 +0800

    Add new model configurations for ControlLDM with various settings
    - Introduced multiple YAML configuration files for ControlLDM models, including cldm_v15_avg_pool.yaml, cldm_v21.yaml, and others.
    - Configurations include parameters for linear start and end, timesteps, image size, channels, and various model-specific settings.
    - Updated control stage configurations to utilize ControlNet with specified parameters such as in_channels, hint_channels, and attention resolutions.
    - Added unet_config and first_stage_config for ControlledUnetModel and AutoencoderKL respectively, ensuring compatibility with the new configurations.
    - Included conditioning configurations using FrozenCLIPEmbedder for enhanced model performance.

--- shiftdm/shitfdm.py
+++ shiftdm/shitfdm.py
@@ -14,7 +14,7 @@ from einops import rearrange, repeat
 from torchvision.utils import make_grid
 from ldm.modules.attention import SpatialTransformer
 from ldm.modules.diffusionmodules.openaimodel import UNetModel, TimestepEmbedSequential, ResBlock, Downsample, AttentionBlock
-from ldm.models.diffusion.ddpm import LatentDiffusion
+from ldm.models.diffusion.ddpm import LatentDiffusion, disabled_train
 from ldm.util import log_txt_as_img, exists, instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 
@@ -30,10 +30,67 @@ class diffusion_example(LatentDiffusion):
 class ShiftLDM(LatentDiffusion):
     """ ShiftNet can inherit any diffusion model """
 
-    def __init__(self, shift_stage_config, shift_key, shift_scale, global_average_pooling=False, *args, **kwargs):
+    def __init__(self, shift_stage_config, shift_stage_key, shift_stage_scale = 1.0, sd_locked = True, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.control_model = instantiate_from_config(control_stage_config)
-        self.control_key = control_key
-        self.only_mid_control = only_mid_control
-        self.control_scales = [1.0] * 13
-        self.global_average_pooling = global_average_pooling
+        self.instantiate_shift_stage(shift_stage_config)
+        self.shift_stage_key = shift_stage_key
+        self.shift_stage_scale = shift_stage_scale
+        self.sd_locked = sd_locked
+
+    def instantiate_shift_stage(self, config):
+        self.shift_stage_model = instantiate_from_config(config)
+
+    def configure_optimizers(self):
+        lr = self.learning_rate
+        params = list(self.control_model.parameters())
+        if not self.sd_locked:
+            params += list(self.model.diffusion_model.output_blocks.parameters())
+            params += list(self.model.diffusion_model.out.parameters())
+        opt = torch.optim.AdamW(params, lr=lr)
+        return opt
+    
+    def configure_optimizers(self):
+        lr = self.learning_rate
+        params = list(self.model.parameters())
+        if self.cond_stage_trainable:
+            print(f"{self.__class__.__name__}: Also optimizing conditioner params!")
+            params = params + list(self.cond_stage_model.parameters())
+        if self.learn_logvar:
+            print('Diffusion model optimizing logvar')
+            params.append(self.logvar)
+        opt = torch.optim.AdamW(params, lr=lr)
+        if self.use_scheduler:
+            assert 'target' in self.scheduler_config
+            scheduler = instantiate_from_config(self.scheduler_config)
+
+            print("Setting up LambdaLR scheduler...")
+            scheduler = [
+                {
+                    'scheduler': LambdaLR(opt, lr_lambda=scheduler.schedule),
+                    'interval': 'step',
+                    'frequency': 1
+                }]
+            return [opt], scheduler
+        return opt
+
+    def encode_shift_stage(self, x_list: list):
+        """
+        Encodes the input using the shift stage model.
+        Args:
+            x_list (list): List of inputs to encode.
+        Returns:
+            torch.Tensor: Encoded output from the shift stage model.
+        """
+        return self.shift_stage_model.encode(x_list)
+
+    def low_vram_shift(self, is_diffusing):
+        if is_diffusing:
+            self.model = self.model.cuda()
+            self.control_model = self.control_model.cuda()
+            self.first_stage_model = self.first_stage_model.cpu()
+            self.cond_stage_model = self.cond_stage_model.cpu()
+        else:
+            self.model = self.model.cpu()
+            self.control_model = self.control_model.cpu()
+            self.first_stage_model = self.first_stage_model.cuda()
+            self.cond_stage_model = self.cond_stage_model.cuda()
